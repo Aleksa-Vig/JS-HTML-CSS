02a8613097594db739e0c8fc442155fbfdad7529
Aleksa-Vig <59575151+Aleksa-Vig@users.noreply.github.com>
2021-01-15 15:25:45 -0500

    Add files via upload

---
+++ getImmunized/home.js
@@ -0,0 +1,4 @@
+let testElement= document.createElement("iframe");
+testElement.id="embMap";
+testElement.src="https://www.google.com/maps/d/u/0/embed?mid=1VO1xC4U4nt_MGq4v5o0_gTPfbj0mx_rT";
+document.getElementById("map").appendChild(testElement);
